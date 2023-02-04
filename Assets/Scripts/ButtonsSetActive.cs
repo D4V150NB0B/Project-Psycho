45dd1bda02ee068b3f8d0678caba52a00216f33d
D4V150NB0B <111165799+D4V150NB0B@users.noreply.github.com>
2023-02-04 17:40:08 -0600

    Reparado

---
+++ Assets/Scripts/ButtonsSetActive.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonsSetActive : MonoBehaviour
+{
+    public GameObject thisObject;
+    public TextAnimated textAnim;
+    public GameObject button1;
+    public GameObject button2;
+    public GameObject button3;
+    bool setted = false;
+    int characters;
+    float timeBtwen;
+
+    float tiempoPaciente;
+    float tiempoParaRespuestas = 10;
+    // Start is called before the first frame update
+    IEnumerator Start()
+    {
+        yield return null;
+
+        characters = textAnim.charCount;
+        timeBtwen = textAnim.timeBtwnChars;
+        Debug.Log(characters);
+
+        tiempoPaciente = characters * timeBtwen;
+        Debug.Log(tiempoPaciente);
+
+        tiempoParaRespuestas = Time.time + tiempoPaciente;
+        Debug.Log(tiempoParaRespuestas);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= tiempoParaRespuestas && setted == false)
+        {
+            ButtonsShow();
+            Debug.Log("ButtonShowed");
+            setted = true;
+        }
+    }
+
+    void ButtonsShow()
+    {
+        button1.SetActive(true);
+        button2.SetActive(true);
+        button3.SetActive(true);
+    }
+  
+}
--- Assets/Scripts/ChatTemplateRobert.cs
+++ Assets/Scripts/ChatTemplateRobert.cs
@@ -3,23 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class chatTemplates : MonoBehaviour
+public class ChatTemplateRobert : MonoBehaviour
 {
     public Button button1;
     public Button button2;
     public Button button3;
     public GameObject canva1;
+    public GameObject canva2;
+    public GameObject canva3;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public void Button1Pressed()
     {
@@ -30,9 +32,13 @@ public class chatTemplates : MonoBehaviour
     public void Button2Pressed()
     {
         Debug.Log(Stats.singleton.confianza);
+        Instantiate(canva2, new Vector3(0, 0, 0), Quaternion.identity);
+        Destroy(transform.gameObject.GetComponentInParent<Canvas>().gameObject);
     }
     public void Button3Pressed()
     {
         Debug.Log(Stats.singleton.motivacion);
+        Instantiate(canva3, new Vector3(0, 0, 0), Quaternion.identity);
+        Destroy(transform.gameObject.GetComponentInParent<Canvas>().gameObject);
     }
 }
