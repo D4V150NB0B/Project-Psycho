43d17d8267e500cfc4e450bbd410ba2d520ca01d
VoidSoulless <69325893+VoidSoulless@users.noreply.github.com>
2023-02-17 12:41:14 -0500

    SpriteSwap 100%

--- Assets/Scripts/SpriteSwap.cs
+++ Assets/Scripts/SpriteSwap.cs
@@ -21,47 +21,47 @@ public class SpriteSwap : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        if ((Stats.singleton.confianza > Stats.singleton.estres) && (Stats.singleton.confianza > Stats.singleton.tension) && (Stats.singleton.confianza > Stats.singleton.motivacion) && Stats.singleton.confianza > 6)
+        if ((Stats.singleton.motivacion >2) && (Stats.singleton.confianza > 2) && (Stats.singleton.estres > 2) && (Stats.singleton.tension > 2) && (Stats.singleton.motivacion < 6) && (Stats.singleton.confianza < 6) && (Stats.singleton.estres < 6) && (Stats.singleton.tension < 6) && ((Stats.singleton.motivacion != Stats.singleton.confianza) || (Stats.singleton.motivacion != Stats.singleton.tension) || (Stats.singleton.motivacion != Stats.singleton.estres)))
         {
-            spriteRenderer.sprite = sOrgullo;
-        }
-        if ((Stats.singleton.estres > Stats.singleton.confianza) && (Stats.singleton.estres > Stats.singleton.tension) && (Stats.singleton.estres > Stats.singleton.motivacion) && Stats.singleton.estres > 6)
-        {
-            spriteRenderer.sprite = sDemacrado;
-        }
-        if ((Stats.singleton.tension > Stats.singleton.confianza) && (Stats.singleton.tension > Stats.singleton.estres) && (Stats.singleton.tension > Stats.singleton.motivacion) && Stats.singleton.tension > 6)
-        {
-            spriteRenderer.sprite = sTristeza;
-        }
-        
-        if ((Stats.singleton.motivacion > Stats.singleton.confianza) && (Stats.singleton.motivacion > Stats.singleton.estres) && (Stats.singleton.motivacion > Stats.singleton.tension) && Stats.singleton.motivacion > 6)
-        {
-            spriteRenderer.sprite = sAlegre;
+            spriteRenderer.sprite = sRelajado;
         }
         else
         {
-            spriteRenderer.sprite = sNeutro;
-        }
-
-        if ((Stats.singleton.motivacion < 6)&& (Stats.singleton.confianza < 6)&& (Stats.singleton.estres < 6)&& (Stats.singleton.tension < 6))
-        {
-            if ((Stats.singleton.confianza == Stats.singleton.estres) || (Stats.singleton.confianza == Stats.singleton.tension) ||
-            (Stats.singleton.confianza == Stats.singleton.motivacion) || (Stats.singleton.tension == Stats.singleton.estres) || (Stats.singleton.motivacion == Stats.singleton.estres) || (Stats.singleton.tension == Stats.singleton.motivacion))
+            //Confianza alta
+            if ((Stats.singleton.confianza > Stats.singleton.motivacion) && (Stats.singleton.confianza > Stats.singleton.estres) && (Stats.singleton.confianza > Stats.singleton.tension) && Stats.singleton.confianza > 6)
             {
-                spriteRenderer.sprite = sNeutro;
+                spriteRenderer.sprite = sOrgullo;
+                Debug.Log("Confianza alta");
             }
-            else
-            {
-                spriteRenderer.sprite = sRelajado;
-            }
-                
-        }
 
-        if ((Stats.singleton.confianza < Stats.singleton.estres) && (Stats.singleton.confianza < Stats.singleton.tension) && (Stats.singleton.confianza < Stats.singleton.motivacion) && Stats.singleton.confianza < 6)
-        {
-            spriteRenderer.sprite = sFaltaDeConfianza;
+
+            //Estres alto
+            if ((Stats.singleton.estres > Stats.singleton.motivacion) && (Stats.singleton.estres > Stats.singleton.confianza) && (Stats.singleton.estres > Stats.singleton.tension) && Stats.singleton.estres > 6)
+            {
+                spriteRenderer.sprite = sDemacrado;
+                Debug.Log("Estres alta");
+            }
+
+            //Tension alta
+            if ((Stats.singleton.tension > Stats.singleton.confianza) && (Stats.singleton.tension > Stats.singleton.estres) && (Stats.singleton.tension > Stats.singleton.motivacion) && Stats.singleton.tension > 6)
+            {
+                spriteRenderer.sprite = sTristeza;
+                Debug.Log("Tension alta");
+            }
+            //Motivacion alta
+            if ((Stats.singleton.motivacion > Stats.singleton.confianza) && (Stats.singleton.motivacion > Stats.singleton.estres) && (Stats.singleton.motivacion > Stats.singleton.tension) && Stats.singleton.motivacion > 6)
+            {
+                spriteRenderer.sprite = sAlegre;
+                Debug.Log("Motivacion alta");
+            }
+
+
+            if ((Stats.singleton.confianza < Stats.singleton.estres) && (Stats.singleton.confianza < Stats.singleton.tension) && (Stats.singleton.confianza < Stats.singleton.motivacion) && Stats.singleton.confianza < 6)
+            {
+                spriteRenderer.sprite = sFaltaDeConfianza;
+            }
         }
+      
     }
 }
 
